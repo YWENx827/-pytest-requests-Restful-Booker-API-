53a1aacf27b72e31a987432d70ef4ebef0a55e02
YWEN <xxtrm619@gmail.com>
2026-09-02 02:44:36 +0800

    重构完第一次

--- tests/test_booking_delete.py
+++ tests/test_booking_delete.py
@@ -1,63 +1,24 @@
-
-
 import requests
 
-def test_delete_booking(token,base_url):
-    payload ={
-    "firstname": "loi",
-    "lastname": "Been",
-    "totalprice": 224,
-    "depositpaid": True,
-    "bookingdates": {
-        "checkin": "2023-02-23",
-        "checkout": "2024-10-23"
-    },
-    "additionalneeds": "Breakfast"
-}
-    resp = requests.post(f"{base_url}/booking",json=payload,timeout=10)
-    booking_id = resp.json()["bookingid"]
-    resp2 = requests.delete(f"{base_url}/booking/{booking_id}",
-                         headers = {"Cookie":f"token={token}"},
-                         timeout=10)
-    assert resp2.status_code == 201
 
-def test_delete_booking_notoken(base_url):
-    payload ={
-    "firstname": "loi",
-    "lastname": "Been",
-    "totalprice": 224,
-    "depositpaid": True,
-    "bookingdates": {
-        "checkin": "2023-02-23",
-        "checkout": "2024-10-23"
-    },
-    "additionalneeds": "Breakfast"
-}
-    resp = requests.post(f"{base_url}/booking",json=payload,timeout=10)
-    booking_id = resp.json()["bookingid"]
-    resp2 = requests.delete(f"{base_url}/booking/{booking_id}",
-                         timeout=10)
-    assert resp2.status_code == 403
-    resp3 = requests.get(f"{base_url}/booking/{booking_id}",timeout=10)
-    assert resp3.status_code == 200
-
-def test_delete_booking_verify(token,base_url):
-    payload ={
-    "firstname": "loi",
-    "lastname": "Been",
-    "totalprice": 224,
-    "depositpaid": True,
-    "bookingdates": {
-        "checkin": "2023-02-23",
-        "checkout": "2024-10-23"
-    },
-    "additionalneeds": "Breakfast"
-}
-    resp = requests.post(f"{base_url}/booking",json=payload,timeout=10)
-    booking_id = resp.json()["bookingid"]
-    resp2 = requests.delete(f"{base_url}/booking/{booking_id}",
-                            headers= {"Cookie":f"token={token}"},
+def test_delete_booking(token, booking, base_url):
+    resp2 = requests.delete(f"{base_url}/booking/{booking}",
+                            headers={"Cookie": f"token={token}"},
                             timeout=10)
     assert resp2.status_code == 201
-    resp3 = requests.get(f"{base_url}/booking/{booking_id}",timeout=10)
+
+
+def test_delete_booking_notoken(booking, base_url):
+    resp2 = requests.delete(f"{base_url}/booking/{booking}", timeout=10)
+    assert resp2.status_code == 403
+    resp3 = requests.get(f"{base_url}/booking/{booking}", timeout=10)
+    assert resp3.status_code == 200
+
+
+def test_delete_booking_verify(token, booking, base_url):
+    resp2 = requests.delete(f"{base_url}/booking/{booking}",
+                            headers={"Cookie": f"token={token}"},
+                            timeout=10)
+    assert resp2.status_code == 201
+    resp3 = requests.get(f"{base_url}/booking/{booking}", timeout=10)
     assert resp3.status_code == 404
